a9dc7040a7a82615cb518a9766c161eaa37bae66
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2026-02-16 17:19:33 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,65 @@
+use std::option;
+
+#[derive(Debug)]
+enum UsState {
+    Albama,
+    Alaska,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
+fn main() {
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    println!("Value is {}", value_in_cents(coin));
+
+    println!("Add Result is: {}", add(50, None));
+
+    let dice_roll = 6;
+    match dice_roll {
+        3 => println!("You got a fancy hat."),
+        6 => println!("Your fancy hat is removed."),
+        other => println!("Move player {}", other),
+    };
+
+}
+
+fn add(num: i32, num2: Option<i32>) -> i32 {
+    match num2 {
+        Some(i) => num + i,
+        None => num,
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("This is Penny");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(UsState::Alaska) => {
+            println!("Hello from Alaska");
+            25
+        }
+        Coin::Quarter(state) => {
+            println!("Got Q of value {:?}", state);
+            25
+
+        }
+    }
+}
+ 
+/* 
 fn main() {
     let op = Some(1);
     let sum = 2 + op.unwrap_or(0);
     println!("Sum is: {sum}");
-}
+} */
 /* 
 #[derive(Debug)]
 enum IpAddKind {
